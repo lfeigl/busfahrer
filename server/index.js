5b45e3ee8cba49a70483a8e9d13231750ec09a78
Luca Feigl <lfeigl.dev@gmail.com>
2021-01-17 02:30:21 +0100

    Enable serverside history mode

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const path = require('path');
+const history = require('connect-history-api-fallback');
 
 const app = express();
 const distDir = path.join(__dirname, '..', 'dist');
 const port = 1337;
 
+app.use(history());
 app.use(express.static(distDir));
 
 app.get('/', (req, res) => {
