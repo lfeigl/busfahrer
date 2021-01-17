ee5f53788036bbc6cdb7f5527f2e5149406f2966
Luca Feigl <lfeigl.dev@gmail.com>
2021-01-17 20:18:31 +0100

    Rewrite server in typescript

--- server/index.ts
+++ server/index.ts
@@ -1,15 +1,15 @@
-const express = require('express');
-const path = require('path');
-const history = require('connect-history-api-fallback');
+import express from 'express';
+import path from 'path';
+import historyApiFallback from 'connect-history-api-fallback';
 
 const app = express();
 const distDir = path.join(__dirname, '..', 'dist');
 const port = 1337;
 
-app.use(history());
+app.use(historyApiFallback());
 app.use(express.static(distDir));
 
-app.get('/', (req, res) => {
+app.get('/', (req: express.Request, res: express.Response) => {
   res.sendFile(path.join(distDir, 'index.html'));
 });
 
