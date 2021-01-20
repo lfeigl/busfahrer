380d502bcd95d58776e042bf88fdaaff989941ea
Luca Feigl <lfeigl.dev@gmail.com>
2021-01-20 23:45:20 +0100

    Add AppBar.vue

---
+++ src/components/AppBar.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-app-bar
+    app
+  >
+    <v-app-bar-title>
+      <v-btn
+        icon
+        large
+        to="/"
+      >
+        <v-icon>mdi-bus</v-icon>
+      </v-btn>
+      Busfahrer
+    </v-app-bar-title>
+
+    <v-spacer/>
+
+    <v-btn
+      icon
+      large
+      to="/about"
+    >
+      <v-icon>mdi-information</v-icon>
+    </v-btn>
+  </v-app-bar>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'AppBar',
+});
+</script>
