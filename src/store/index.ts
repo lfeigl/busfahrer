0039979b6bce7c5069d6ac268b67ba1fcdf2760b
Luca Feigl <lfeigl.dev@gmail.com>
2021-01-23 18:28:59 +0100

    Add vuex user state & mutations

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,15 +1,24 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import { StoreState } from '@/types';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-  },
+    user: {
+      id: '',
+      name: '',
+    },
+  } as StoreState,
   mutations: {
+    setUserId(state: StoreState, userId: string): void {
+      state.user.id = userId;
+    },
+    setUsername(state: StoreState, username: string): void {
+      state.user.name = username;
+    },
   },
   actions: {
-  },
-  modules: {
   },
 });
