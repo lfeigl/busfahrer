d0d4743634d59144ba0eea58552d09d5f0936123
Luca Feigl <lfeigl.dev@gmail.com>
2021-01-23 17:45:29 +0100

    Capitalize title generically

--- vue.config.js
+++ vue.config.js
@@ -9,8 +9,16 @@ module.exports = {
     config
       .plugin('html')
       .tap((args) => {
+        let { title } = args[0];
+
+        title = title
+          .split('-')
+          .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+          .join(' ');
+
         // eslint-disable-next-line no-param-reassign
-        args[0].title = 'Busfahrer';
+        args[0].title = title;
+
         return args;
       });
   },
