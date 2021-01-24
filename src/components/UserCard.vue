2503831d165d11d598c0c7d8b1a7bf01247cfb01
Luca Feigl <lfeigl.dev@gmail.com>
2021-01-24 17:14:28 +0100

    Add UserCard.vue

---
+++ src/components/UserCard.vue
@@ -0,0 +1,122 @@
+<template>
+  <v-card>
+    <v-card-title class="headline">
+      <span>{{ user.name || '...' }}</span>
+
+      <v-spacer/>
+
+      <v-btn
+        icon
+        @click="resetUsername"
+      >
+        <v-icon>mdi-account-edit</v-icon>
+      </v-btn>
+    </v-card-title>
+
+    <v-card-text>
+      <v-list>
+        <v-row dense>
+          <v-col
+            v-for="(statGroup, index) in statGroups"
+            :key="index"
+            cols="12"
+          >
+            <v-row
+              dense
+              align="center"
+            >
+              <v-col cols="1">
+                <span class="display-1">
+                  {{ statGroup.icon }}
+                </span>
+              </v-col>
+              <v-col
+                v-for="stat in statGroup.stats"
+                :key="stat.key"
+                cols="5.5"
+              >
+                <v-list-item>
+                  <v-list-item-content>
+                    <v-list-item-title class="headline">{{ stat.value }}</v-list-item-title>
+                    <v-list-item-subtitle class="subtitle-1">{{ stat.name }}</v-list-item-subtitle>
+                  </v-list-item-content>
+                </v-list-item>
+              </v-col>
+            </v-row>
+            <v-divider/>
+          </v-col>
+        </v-row>
+      </v-list>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import vuex from 'vuex';
+
+export default Vue.extend({
+  name: 'UserCard',
+  computed: {
+    ...vuex.mapState([
+      'user',
+    ]),
+  },
+  methods: {
+    resetUsername(): void {
+      this.user.name = null;
+    },
+  },
+  data() {
+    return {
+      statGroups: [
+        {
+          icon: '🚍',
+          stats: [
+            {
+              key: 'totalFahrten',
+              name: 'Fahrten',
+              value: 0,
+            },
+            {
+              key: 'totalBusfahrer',
+              name: 'davon Busfahrer gewesen',
+              value: 0,
+            },
+          ],
+        },
+        {
+          icon: '🍺',
+          stats: [
+            {
+              key: 'totalGulpsReceived',
+              name: 'Schlücke bekommen',
+              value: 0,
+            },
+            {
+              key: 'averageGulpsReceived',
+              name: '∅ Schlücke pro Fahrt',
+              value: 0,
+            },
+          ],
+        },
+        {
+          icon: '🍻',
+          stats: [
+            {
+              key: 'totalGulpsDispensed',
+              name: 'Schlücke verteilt',
+              value: 0,
+            },
+            {
+              key: 'averageGulpsDispensed',
+              name: '∅ Schlücke pro Fahrt',
+              value: 0,
+            },
+          ],
+        },
+      ],
+    };
+  },
+});
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,24 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-  </div>
+  <v-container fluid>
+    <v-row>
+      <v-col cols="7">
+
+      </v-col>
+      <v-col cols="5">
+        <UserCard/>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
+import UserCard from '@/components/UserCard.vue';
 
 export default Vue.extend({
   name: 'Home',
+  components: {
+    UserCard,
+  },
 });
 </script>
