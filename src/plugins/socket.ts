05d2675e88c809d64257997ddf5effcc3c5df792
Luca Feigl <lfeigl.dev@gmail.com>
2021-01-29 23:13:22 +0100

    Transform mutation event names to constant case ("SOCKET_EVENT_NAME") & action event names to camel case ("socketEventName")

--- src/plugins/socket.ts
+++ src/plugins/socket.ts
@@ -1,8 +1,12 @@
 import Vue from 'vue';
 import VueSocketIOExt from 'vue-socket.io-extended';
 import socketIOClient from 'socket.io-client';
+import { constantCase, pascalCase } from 'change-case';
 import store from '@/store';
 
 Vue.use(VueSocketIOExt, socketIOClient.io(), {
   store,
+  actionPrefix: 'socket',
+  eventToMutationTransformer: (eventName: string): string => constantCase(eventName),
+  eventToActionTransformer: (eventName: string): string => pascalCase(eventName),
 });
