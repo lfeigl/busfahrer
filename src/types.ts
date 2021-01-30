20318ad1108fb8b1c9f68b9c2e38de7e4a5b89fc
Luca Feigl <lfeigl.dev@gmail.com>
2021-01-30 19:11:44 +0100

    Add room type

--- src/types.ts
+++ src/types.ts
@@ -3,6 +3,7 @@ import { ActionContext } from 'vuex';
 export type StoreState = {
   theme: string;
   player: Player;
+  room: Room;
 };
 
 export type StoreActionContext = ActionContext<StoreState, StoreState>;
@@ -10,4 +11,12 @@ export type StoreActionContext = ActionContext<StoreState, StoreState>;
 export type Player = {
   id: string;
   name: string;
+};
+
+export type Room = {
+  id: string;
+  name: string;
+  owner: string;
+  isOpen: boolean;
+  players: Player[];
 };
