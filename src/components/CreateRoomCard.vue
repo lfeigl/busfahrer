1e8769cb7106cd2b744780158f02229e142565ec
Luca Feigl <lfeigl.dev@gmail.com>
2021-01-31 19:02:06 +0100

    Add CreateRoomCard.vue

---
+++ src/components/CreateRoomCard.vue
@@ -0,0 +1,66 @@
+<template>
+  <v-card>
+    <v-card-title class="headline">
+      🚍 Neue Busfahrt
+    </v-card-title>
+
+    <v-card-text>
+      <v-text-field
+        v-model="roomName"
+        counter
+        maxlength="25"
+        label="Name der Buslinie"
+        hide-details="auto"
+      />
+    </v-card-text>
+
+    <v-card-actions>
+      <v-spacer/>
+      <v-btn
+        color="primary"
+        text
+        :disabled="isEmptyString(roomName)"
+        @click="createRoom"
+      >
+        Türen öffnen
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import vuex from 'vuex';
+import { Room } from '@/types';
+import { isEmptyString } from '@/utils';
+
+export default Vue.extend({
+  name: 'CreateRoomCard',
+  data() {
+    return {
+      roomName: '',
+    };
+  },
+  computed: {
+    ...vuex.mapState([
+      'player',
+    ]),
+  },
+  methods: {
+    isEmptyString,
+    createRoom(): void {
+      this.$socket.client.emit('createRoom', {
+        player: this.player,
+        roomName: this.roomName,
+      }, (room: Room) => {
+        this.$router.push({
+          name: 'Join',
+          params: {
+            roomId: room.id,
+          },
+        });
+      });
+    },
+  },
+});
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,10 @@
 <template>
   <v-container fluid>
     <v-row>
-      <v-col cols="7">
-
+      <v-col cols="6">
+        <CreateRoomCard/>
       </v-col>
-      <v-col cols="5">
+      <v-col cols="6">
         <PlayerCard/>
       </v-col>
     </v-row>
@@ -13,11 +13,13 @@
 
 <script lang="ts">
 import Vue from 'vue';
+import CreateRoomCard from '@/components/CreateRoomCard.vue';
 import PlayerCard from '@/components/PlayerCard.vue';
 
 export default Vue.extend({
   name: 'Home',
   components: {
+    CreateRoomCard,
     PlayerCard,
   },
 });
