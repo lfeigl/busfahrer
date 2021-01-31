1488bc3e66bd8ae823cc50e4732e2438fde76062
Luca Feigl <lfeigl.dev@gmail.com>
2021-01-31 21:39:14 +0100

    Add JoinRoomCard.vue

---
+++ src/components/JoinRoomCard.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-card>
+    <v-card-title class="headline">
+      🚍 Einsteigen
+    </v-card-title>
+
+    <v-card-text>
+      Lass dir einen Link zusenden, um in einen Bus einzusteigen:
+      <code>{{ exampleJoinLink }}</code>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'JoinRoomCard',
+  computed: {
+    exampleJoinLink(): string {
+      return window.location.origin + this.$router.resolve({
+        name: 'Join',
+        params: {
+          roomId: '...',
+        },
+      }).href;
+    },
+  },
+});
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,14 @@
   <v-container fluid>
     <v-row>
       <v-col cols="6">
-        <CreateRoomCard/>
+        <v-row>
+          <v-col cols="12">
+            <JoinRoomCard/>
+          </v-col>
+          <v-col cols="12">
+            <CreateRoomCard/>
+          </v-col>
+        </v-row>
       </v-col>
       <v-col cols="6">
         <PlayerCard/>
@@ -13,12 +20,14 @@
 
 <script lang="ts">
 import Vue from 'vue';
+import JoinRoomCard from '@/components/JoinRoomCard.vue';
 import CreateRoomCard from '@/components/CreateRoomCard.vue';
 import PlayerCard from '@/components/PlayerCard.vue';
 
 export default Vue.extend({
   name: 'Home',
   components: {
+    JoinRoomCard,
     CreateRoomCard,
     PlayerCard,
   },
