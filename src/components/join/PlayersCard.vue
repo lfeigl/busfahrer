62904f8ba345b235d6a6698e4b33925a938e8d0b
Luca Feigl <lfeigl.dev@gmail.com>
2021-02-02 19:44:19 +0100

    Add PlayersCard.vue

---
+++ src/components/join/PlayersCard.vue
@@ -0,0 +1,48 @@
+<template>
+  <v-card>
+    <v-card-title class="headline">
+      Mitfahrer
+    </v-card-title>
+
+    <v-card-text>
+      <v-list>
+        <v-list-item
+          v-for="player in room.players"
+          :key="player.id"
+        >
+          <v-list-item-icon>
+            <v-icon
+              color="primary"
+              v-if="room.owner.id === player.id"
+            >
+              mdi-account-star
+            </v-icon>
+            <v-icon v-else>
+              mdi-account
+            </v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>
+              {{ player.name }}
+            </v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import vuex from 'vuex';
+
+export default Vue.extend({
+  name: 'PlayersCard',
+  computed: {
+    ...vuex.mapState([
+      'room',
+    ]),
+  },
+});
+</script>
