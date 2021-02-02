6197633c53660b50ead2284e5a911d8cf6a52261
Luca Feigl <lfeigl.dev@gmail.com>
2021-02-02 19:46:05 +0100

    Add ControlCard.vue

---
+++ src/components/join/ControlCard.vue
@@ -0,0 +1,70 @@
+<template>
+  <v-card>
+    <v-card-title class="headline">
+      Einsteigen in Buslinie "{{ room.name }}"
+    </v-card-title>
+
+    <v-card-text>
+      Mitfahrer können über diesen Link einsteigen:
+      <code>{{ joinLink }}</code>
+    </v-card-text>
+
+    <v-card-actions>
+      <v-btn
+        color="error"
+        text
+        @click="leaveRoom"
+      >
+        Aussteigen
+      </v-btn>
+
+      <v-spacer/>
+
+      <v-btn
+        large
+        text
+        v-clipboard="joinLink"
+      >
+        Link kopieren
+      </v-btn>
+
+      <v-spacer/>
+
+      <v-btn
+        x-large
+        color="primary"
+        text
+        @click="startGame"
+      >
+        Losfahren
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import vuex from 'vuex';
+
+export default Vue.extend({
+  name: 'ControlCard',
+  computed: {
+    ...vuex.mapState([
+      'room',
+    ]),
+    joinLink(): string {
+      return window.location.origin + this.$route.path;
+    },
+  },
+  methods: {
+    leaveRoom(): void {
+      this.$router.push({
+        name: 'Home',
+      });
+    },
+    startGame(): void {
+      console.log(this.room);
+    },
+  },
+});
+</script>
