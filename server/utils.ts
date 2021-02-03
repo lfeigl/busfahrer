60b32897583ea07e6169b3d54c7a2e375bc018e4
Luca Feigl <lfeigl.dev@gmail.com>
2021-02-03 23:02:32 +0100

    Add arg "log" for writing a log file

--- server/utils.ts
+++ server/utils.ts
@@ -1,4 +1,15 @@
-// eslint-disable-next-line import/prefer-default-export
+import { join as joinPath } from 'path';
+import { appendFileSync } from 'fs';
+import argv from './argv';
+
+export function writeToLogFile(...messages: string[]): void {
+  const logPath = joinPath(__dirname, '..', 'busfahrer.log');
+  const logMessage = `${messages.join(' ')}\n`;
+
+  appendFileSync(logPath, logMessage);
+}
+
 export function log(...messages: string[]): void {
   console.log('[Busfahrer]', ...messages);
+  if (argv.log) writeToLogFile(...messages);
 }
