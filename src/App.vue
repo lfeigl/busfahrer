2abf714f82074649ebf721837cf7440b10a7eef9
Luca Feigl <lfeigl.dev@gmail.com>
2021-02-06 18:33:57 +0100

    Add Footer.vue

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,8 @@
       <WelcomeDialog/>
       <router-view v-if="playerName"/>
     </v-main>
+
+    <Footer/>
   </v-app>
 </template>
 
@@ -14,12 +16,14 @@ import Vue from 'vue';
 import vuex from 'vuex';
 import AppBar from '@/components/common/AppBar.vue';
 import WelcomeDialog from '@/components/common/WelcomeDialog.vue';
+import Footer from '@/components/common/Footer.vue';
 
 export default Vue.extend({
   name: 'App',
   components: {
     AppBar,
     WelcomeDialog,
+    Footer,
   },
   computed: {
     ...vuex.mapState([
---
+++ src/components/common/Footer.vue
@@ -0,0 +1,68 @@
+<template>
+  <v-footer padless>
+    <v-row no-gutters>
+      <v-col
+        cols="4"
+        class="text-left"
+      >
+        <v-btn
+          text
+          :href="busfahrerApp.homepage"
+          target="_blank"
+        >
+          <v-icon left>mdi-code-tags</v-icon>
+          Contribute
+        </v-btn>
+      </v-col>
+
+      <v-col
+        cols="4"
+        class="text-center"
+      >
+        <span class="font-weight-light body-2">
+          <strong>{{ busfahrerApp.name }} v{{ busfahrerApp.version }}</strong>
+          by {{ busfahrerApp.author }}
+        </span>
+      </v-col>
+
+      <v-col
+        cols="4"
+        class="text-right"
+      >
+        <v-btn
+          text
+          :href="aidPage"
+          target="_blank"
+        >
+          <v-icon left>mdi-lifebuoy</v-icon>
+          Alkohol? Kenn dein Limit.
+        </v-btn>
+      </v-col>
+    </v-row>
+  </v-footer>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import vuex from 'vuex';
+
+export default Vue.extend({
+  name: 'Footer',
+  computed: {
+    ...vuex.mapState([
+      'busfahrerApp',
+    ]),
+  },
+  data() {
+    return {
+      aidPage: 'https://www.kenn-dein-limit.de',
+    };
+  },
+});
+</script>
+
+<style scoped>
+.text-center {
+  padding-top: 6px;
+}
+</style>
