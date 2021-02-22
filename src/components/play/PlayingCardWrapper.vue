a10683ddfddcab389fe4c9d05388bf1cc1ef3324
Luca Feigl <lfeigl.dev@gmail.com>
2021-02-22 23:51:10 +0100

    Add PlayingCardWrapper.vue

---
+++ src/components/play/PlayingCardWrapper.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-img
+    class="ma-1"
+    contain
+    :src="require(`@/assets/cards/suits/${suit}/${playingCard.fileName}`)"
+    :max-width="maxWidth"
+  />
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import { playingCards } from '@/utils';
+
+export default Vue.extend({
+  name: 'PlayingCardWrapper',
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+    suit: {
+      type: String,
+      required: true,
+    },
+    maxWidth: {
+      type: Number,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      playingCard: playingCards[this.name],
+    };
+  },
+});
+</script>
