31c150b78ef94171d30467b24f08ce7025268b71
Luca Feigl <lfeigl.dev@gmail.com>
2021-02-22 23:43:38 +0100

    Add playing cards mapping util

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,73 @@
-// eslint-disable-next-line import/prefer-default-export
+import { PlayingCard } from '@/types';
+
 export function isEmptyString(string: string): boolean {
   return string.replaceAll(' ', '').length === 0;
 }
+
+export const playingCards: { [name: string]: PlayingCard } = {
+  ace: {
+    name: 'ace',
+    value: 100,
+    fileName: '01.svg',
+  },
+  two: {
+    name: 'two',
+    value: 2,
+    fileName: '02.svg',
+  },
+  three: {
+    name: 'three',
+    value: 3,
+    fileName: '03.svg',
+  },
+  four: {
+    name: 'four',
+    value: 4,
+    fileName: '04.svg',
+  },
+  five: {
+    name: 'five',
+    value: 5,
+    fileName: '05.svg',
+  },
+  six: {
+    name: 'six',
+    value: 6,
+    fileName: '06.svg',
+  },
+  seven: {
+    name: 'seven',
+    value: 7,
+    fileName: '07.svg',
+  },
+  eight: {
+    name: 'eight',
+    value: 8,
+    fileName: '08.svg',
+  },
+  nine: {
+    name: 'nine',
+    value: 9,
+    fileName: '09.svg',
+  },
+  ten: {
+    name: 'ten',
+    value: 10,
+    fileName: '10.svg',
+  },
+  jack: {
+    name: 'jack',
+    value: 25,
+    fileName: '11.svg',
+  },
+  queen: {
+    name: 'queen',
+    value: 50,
+    fileName: '12.svg',
+  },
+  king: {
+    name: 'king',
+    value: 75,
+    fileName: '13.svg',
+  },
+};
