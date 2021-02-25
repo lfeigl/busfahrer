85e40750f1c6e6bd90b78ae8d316ce6e73ad2d67
Luca Feigl <lfeigl.dev@gmail.com>
2021-02-25 21:52:04 +0100

    Add Fir.vue

---
+++ src/components/play/Fir.vue
@@ -0,0 +1,65 @@
+<template>
+  <div :id="containerId">
+    <div
+      v-for="(row, i) in playingCards"
+      :key="i"
+      class="d-flex justify-center"
+    >
+      <PlayingCard
+        v-for="(card, j) in row"
+        :key="j"
+        :name="card.name"
+        :suit="card.suit"
+        :max-width="playingCardMaxWidth"
+      />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import PlayingCard from '@/components/play/PlayingCard.vue';
+import PlayingCardMaxWidth from '@/mixins/playing-card-max-width';
+
+export default Vue.extend({
+  name: 'Fir',
+  components: {
+    PlayingCard,
+  },
+  mixins: [
+    PlayingCardMaxWidth,
+  ],
+  data() {
+    return {
+      containerId: 'fir',
+      playingCards: [
+        [
+          {},
+        ],
+        [
+          {},
+          {},
+        ],
+        [
+          {},
+          {},
+          {},
+        ],
+        [
+          {},
+          {},
+          {},
+          {},
+        ],
+        [
+          {},
+          {},
+          {},
+          {},
+          {},
+        ],
+      ],
+    };
+  },
+});
+</script>
