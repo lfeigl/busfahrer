6daafbe009a6a48b15e9ef9ead5ae2e496e8c190
Luca Feigl <lfeigl.dev@gmail.com>
2021-03-21 20:34:19 +0100

    Add fir formatting

--- src/components/play/Fir.vue
+++ src/components/play/Fir.vue
@@ -1,11 +1,11 @@
 <template>
   <div :id="containerId">
     <div
-      v-for="(row, i) in playingCards"
+      v-for="(row, i) in fir"
       :key="i"
       class="d-flex justify-center"
     >
-      <PlayingCard
+      <PlayingCardComponent
         v-for="(card, j) in row"
         :key="j"
         :name="card.name"
@@ -18,13 +18,15 @@
 
 <script lang="ts">
 import Vue from 'vue';
-import PlayingCard from '@/components/play/PlayingCard.vue';
+import vuex from 'vuex';
+import PlayingCardComponent from '@/components/play/PlayingCard.vue';
 import PlayingCardMaxWidth from '@/mixins/playing-card-max-width';
+import { PlayingCard } from '@/types';
 
 export default Vue.extend({
   name: 'Fir',
   components: {
-    PlayingCard,
+    PlayingCardComponent,
   },
   mixins: [
     PlayingCardMaxWidth,
@@ -32,34 +34,31 @@ export default Vue.extend({
   data() {
     return {
       containerId: 'fir',
-      playingCards: [
-        [
-          {},
-        ],
-        [
-          {},
-          {},
-        ],
-        [
-          {},
-          {},
-          {},
-        ],
-        [
-          {},
-          {},
-          {},
-          {},
-        ],
-        [
-          {},
-          {},
-          {},
-          {},
-          {},
-        ],
-      ],
+      playingCardRowCount: 5,
     };
+  },
+  computed: {
+    ...vuex.mapState([
+      'room',
+    ]),
+    fir(): PlayingCard[][] {
+      const fir: PlayingCard[][] = [];
+      const firCards = [...this.room.firCards];
+      let end = 0;
+
+      while (firCards.length < 15) {
+        firCards.push({});
+      }
+
+      for (let i = 5; i >= 1; i -= 1) {
+        const start = end;
+        end = start + i;
+        const row = firCards.slice(start, end);
+        fir.push(row);
+      }
+
+      return fir.reverse();
+    },
   },
 });
 </script>
