e3162a514b2d4e0eb633e954a911df815876f61e
Luca Feigl <lfeigl.dev@gmail.com>
2021-03-21 15:46:50 +0100

    Add deck logic

---
+++ server/game/deck.ts
@@ -0,0 +1,64 @@
+import { PlayingCard } from './types';
+
+const names = [
+  'ace',
+  'two',
+  'three',
+  'four',
+  'five',
+  'six',
+  'seven',
+  'eight',
+  'nine',
+  'ten',
+  'jack',
+  'queen',
+  'king',
+];
+
+const suits = [
+  'clubs',
+  'diamonds',
+  'hearts',
+  'spades',
+];
+
+const values: {
+  [name: string]: number,
+} = {
+  ace: 100,
+  two: 2,
+  three: 3,
+  four: 4,
+  five: 5,
+  six: 6,
+  seven: 7,
+  eight: 8,
+  nine: 9,
+  ten: 10,
+  jack: 25,
+  queen: 50,
+  king: 75,
+};
+
+const fullDeck = ((): PlayingCard[] => {
+  const playingCards: PlayingCard[] = [];
+
+  names.forEach((name) => {
+    suits.forEach((suit) => {
+      const playingCard: PlayingCard = {
+        name,
+        suit,
+        value: values[name],
+      };
+
+      playingCards.push(playingCard);
+    });
+  });
+
+  return playingCards;
+})();
+
+export default function getFullDeck(): PlayingCard[] {
+  return [...fullDeck];
+}
