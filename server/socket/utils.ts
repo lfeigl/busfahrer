e1d71069d6fd4f3d929939f87eae75b8c86ab80f
Luca Feigl <lfeigl.dev@gmail.com>
2021-05-04 22:54:17 +0200

    Add checkIfPlayerIsInRoom & checkIfPlayerHasCard utils

--- server/socket/utils.ts
+++ server/socket/utils.ts
@@ -1,6 +1,7 @@
 import { log } from '../utils';
 import rooms from './rooms';
 import { PlayerSocket } from './types';
+import { PlayingCard } from '../game/types';
 
 type LeaveRoomEvent = (socket: PlayerSocket, roomId: string) => void;
 
@@ -8,18 +9,52 @@ export function cleanRooms(): void {
   Object.keys(rooms).forEach((roomId: string) => {
     const room = rooms[roomId];
 
-    if (!Object.keys(room.players).length) {
-      delete rooms[roomId];
-      log(`Server removed empty room "${room.name}" (${room.id}).`);
+    if (room) {
+      if (!Object.keys(room.players).length) {
+        delete rooms[roomId];
+        log(`Server removed empty room "${room.name}" (${room.id}).`);
+      }
     }
   });
 }
 
-export function leaveJoinedRooms(socket: PlayerSocket, callback: LeaveRoomEvent): void {
+export function checkIfPlayerIsInRoom(socket: PlayerSocket): boolean {
   const { player } = socket;
+  const { roomId } = player;
 
+  if (roomId) {
+    const room = rooms[roomId];
+
+    if (room) {
+      return Object.keys(room.players).includes(player.id);
+    }
+  }
+
+  return false;
+}
+
+export function checkIfPlayerHasCard(socket: PlayerSocket, card: PlayingCard): boolean {
+  const { player } = socket;
+  const { roomId } = player;
+
+  if (roomId) {
+    const room = rooms[roomId];
+
+    if (room) {
+      return room.players[player.id].hand?.includes(card) || false;
+    }
+  }
+
+  return false;
+}
+
+export function validatePlay(socket: PlayerSocket, card: PlayingCard): boolean {
+  return checkIfPlayerIsInRoom(socket) && checkIfPlayerHasCard(socket, card);
+}
+
+export function leaveJoinedRooms(socket: PlayerSocket, callback: LeaveRoomEvent): void {
   Object.keys(rooms).forEach((roomId: string) => {
-    if (Object.keys(rooms[roomId].players).includes(player.id)) {
+    if (checkIfPlayerIsInRoom(socket)) {
       callback(socket, roomId);
     }
   });
